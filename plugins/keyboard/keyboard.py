e4ac49bdd8e7b97bc65493214a847751930997fd
Alex Caranha <alex.caranha@gmail.com>
2020-10-24 20:24:53 -0300

    Plugin recorder created.

--- plugins/keyboard/keyboard.py
+++ plugins/keyboard/keyboard.py
@@ -1,19 +1,16 @@
 from yapsy.IPlugin import IPlugin
 from plugins.categories import Internal
-import plugins.keyboard.module
+import plugins.keyboard.module as module
 from fuzzywuzzy import fuzz
 
 class Keyboard(Internal):
     def setup(self, parent):
         self.parent = parent        
-        self.__activate__(False)
+        self.activated = False
         print(f"{parent.name} loaded: ok.")
 
-    def __activate__(self, value):
-        self.activated = value
-
     def write(self, sentence:str):
-        return write_sentence(sentence)
+        return module.write_sentence(sentence)
 
     def is_the_question(self, pattern, input: str, ratio_threshold = 85):
         ratio = fuzz.token_set_ratio(pattern, input)
@@ -50,13 +47,13 @@ class Keyboard(Internal):
     def run(self, input):
         if self.activated:
             if self.is_the_question(r'sair do modo escrita', input, 95):
-                self.__activate__(False)
+                self.activated = False
                 return "Você saiu do plugin de escrever."
 
             return self.commands_in_normal_mode(input)
 
         if self.is_the_question(r'entrar no modo escrita', input, 95):
-            self.__activate__(True)
+            self.activated = True
             return None
 
         return None
